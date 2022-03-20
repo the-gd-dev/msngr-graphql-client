797e02130ff751756e8faef2bf27aaa2ae7f2d31
Ghanshyam Dutt <ghanshyamdutt1407@gmail.com>
2022-03-20 22:31:19 +0530

    conversations and create message

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -4,7 +4,10 @@ import ProfilePicture from "./ProfilePicture";
 const SearchResult = (props) => {
   const { user } = props;
   return (
-    <div onClick={() => props.newPersonSelected(user)} className="flex space-x-3 items-center hover:bg-gray-100 rounded-md py-2 px-4 cursor-pointer">
+    <div
+      onClick={props.newPersonSelected}
+      className="flex space-x-3 items-center hover:bg-gray-100 rounded-md py-2 px-4 cursor-pointer"
+    >
       <ProfilePicture
         style={{
           height: 45,
